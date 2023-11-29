7495fa4e80accdd29bca4b3e5a78ab8b47f93946
Tiago <a106883@alunos.uminho.pt>
2023-11-29 16:51:50 +0000

    Tarefa 1 e 2

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,14 +10,16 @@ module Tarefa1 where
 import LI12324
 import Data.Fixed (E0)
 
+
+
 colisoesParede :: Mapa -> Personagem -> Bool
-colisoesParede (Bloco Mapa) Posicao Personagem | x == limite || y == limite = True
-                                               | sobreposicao (posicao personagem) (espacoBloco (0,0) (Bloco Mapa))
+colisoesParede (Bloco Mapa) posicao Personagem | x == 100 || y == 100 = True
+                                               | sobreposicao (posicao personagem) (espacoBloco (0,0) (Bloco Mapa)) = True
                                                | otherwise = False
 
 
 colisoesPersonagens :: Personagem -> [Personagem] -> Bool
-colisoesPersonagens | sobreposicao ((posicao personagem), (posicao inimigos)) = True 
+colisoesPersonagens | colisao ((posicao personagem), (posicao inimigos)) = True 
                     | otherwise = False
 
 
@@ -44,13 +46,25 @@ sobreposicao :: Hitbox -> Hitbox -> Bool
 sobreposicao ((p1,p2),(p3,p4)) ((p5,p6),(p7,p8)) | (p1 <= p5 && p5 <= p3 || p1 <= p7 && p7<= p3 ) && (p2 <= p6 && p6 <= p4 || p2 <= p8 && p8<= p4 ) = True
                                                  | otherwise = False 
 
+{-| Função que testa se duas Hitboxs estão a colidir.
 
+== Exemplos
+
+>>>colisao ((1,1),(2,2))  [((1.5,1.5),(3.5,3.5)), ((1.5,1.5),(3.5,3.5))]
+True
+-}
+
+
+colisao :: Hitbox -> [Hitbox] -> Bool
+colisao _ [] = True
+colisao ((p1,p2),(p3,p4)) (((p5,p6),(p7,p8)):t) | sobreposicao ((p1,p2),(p3,p4)) ((p5,p6),(p7,p8)) == False = False
+                                                | otherwise = colisaoc ((p1,p2),(p3,p4)) t
 
 {-| Função que dada uma posição inicial e uma lista de listas de Blocos(cada lista de Blocos corresponde a uma linha ) dá a posição de todos os Blocos.
 
 == Exemplos
 
->>> posicaoBlocos (0,0) [[Vazio,Escada,Plataforma],[Vazio,Escada,Plataforma]]
+>>> posicaoBlocos (0.0,0.0) [[Vazio,Escada,Plataforma],[Vazio,Escada,Plataforma]]
 [(1.0,0.0),(2.0,0.0),(1.0,1.0),(2.0,1.0)]
 -}
 posicaoBlocos :: Posicao -> [[Bloco]] -> [Posicao]
@@ -63,7 +77,7 @@ posicaoBlocos (x, y) (h:t) = posicaoBlocop (x, y) h ++ posicaoBlocos (x, y + 1) 
 
 == Exemplos
 
->>> posicaoBlocop (0,0) [Vazio,Escada,Plataforma]
+>>> posicaoBlocop (0.0,0.0) [Vazio,Escada,Plataforma]
 [(1.0,0.0),(2.0,0.0)]
 -}
 posicaoBlocop :: Posicao -> [Bloco] -> [Posicao]
@@ -73,7 +87,7 @@ posicaoBlocop (x, y) (h:t)
     | otherwise = (x, y) : posicaoBlocop (x + 1, y) t
 
 
-{-| Função que dada uma posição inicial e uma lista de Blocos dá a posição desses Blocos.
+{-| Função que dada uma posição inicial e uma lista de blocos dá a hitbox desses blocos
 
 == Exemplos
 
@@ -84,6 +98,14 @@ espacoBloco :: Posicao -> [[Bloco]] -> [Hitbox]
 espacoBloco _ [] = []
 espacoBloco (x,y) (h:t) = ((posicaoBlocos (x,y) (Bloco Mapa)), (posicaoBlocos (x + 1,y + 1) (Bloco Mapa))) : espacoBloco (x,y) t 
 
+
+{-| Função que dada a posiçao de uma personagem e o seu tamanho cria a sua hitbox.
+
+== Exemplos
+
+>>> 
+
+-}
 criaHitbox :: Posicao -> Personagem -> Hitbox
 criaHitbox (x,y) tamanho Personagem = ((x,y), (x + (fst (tamanho Personagem ))), (y + (snd (tamanho Personagem ))))
 
--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -7,9 +7,9 @@ Copyright   : José Miguel Torrinha Paredes Pinho Sampaio <a106908@alunos.uminho
 Módulo para a realização da Tarefa 2 de LI1 em 2023/24.
 -}
 module Tarefa2 where
-
+import Tarefa1
 import LI12324
-{-}
+
 data Jogo =
   Jogo
     { mapa          :: Mapa -- ^ mapa do jogo
@@ -20,21 +20,14 @@ data Jogo =
   deriving (Eq, Read, Show)
 
 valida :: Jogo -> Bool
-<<<<<<< HEAD
-valida jogo    | inimigos ressalta == True && jogador ressalata == False = False 
-               | jogador posicao == inimigos posicao = False
-               | = False
-               | inimigos vida \= 1 = False
-               |= False
+valida jogo    | ressalta inimigos == True &&  ressalata jogador == False = False 
+               | posicao jogador == posicao inimigos = False
+               | validaesc == False = False
+               | vida inimigos \= 1 = False
+               | colisoesParede == False = False
                |= False
                |= False
                | otherwise = True
--}
-
-               
-               
-=======
-valida jogo = undefined
 
 --validaressalta :: Personagem -> [Personagem] -> Bool 
 --validaressalta jog = not (ressalta jog) &&  
@@ -43,6 +36,137 @@ validachao :: Mapa -> Bool
 validachao (Mapa _ _ matriz) = all (==Plataforma) (last matriz)
 
 
+emBloco :: [Posicao] -> [Posicao] -> Bool
 
 
->>>>>>> 7c680f362ebaa823421dca5a9277366c5603c4e4
+----- validar escadas(por acabar)
+
+
+{-| Função que dada uma posição inicial e uma lista de listas de Blocos(cada lista de Blocos corresponde a uma linha ) dá a posição de todas as escadas.
+
+== Exemplos
+
+>>>
+-}
+
+
+validaesc :: Mapa -> Bool
+validaesc (Bloco Mapa) | validacoisasal (posicaoBlocosesc) (posicaoBlocosal) == False = False
+                       | validacoisasplat (posicaoBlocosesc) (posicaoBlocosplat) == False = False
+                       | otherwise = True  
+
+
+                                 
+
+validaCoisasPlat :: [Posicao] -> [Posicao] -> Bool
+validaCoisasPlat [] [] = True
+validaCoisasPlat [] _ = True
+validaCoisasPlat _ [] = False
+validaCoisasPlat ((h,r):t) ((x,z):y) | (h,r) == (x,z+1) || (h,r-1) == (x,z) = validaCoisasPlat t ((x,z):y)
+                                     |otherwise = validaCoisasPlat ((h,r):t) y
+
+
+validaCoisasAl :: [Posicao] -> [Posicao] -> Bool
+validaCoisasAl [] [] = True
+validaCoisasAl [] _ = True
+validaCoisasAl _ [] = False
+validaCoisasAl ((h,r):t) ((x,z):y) | (h,r) /= (x,z+1) && (h,r-1) /= (x,z) = validaCoisasAl t ((x,z):y)
+                                   | otherwise = False
+
+
+
+-----Feito
+
+{-| Função que dada uma posição inicial e uma lista de listas de Blocos(cada lista de Blocos corresponde a uma linha ) dá a posição de todas as escadas.
+
+== Exemplos
+
+>>> posicaoBlocosesc (0.0,0.0) [[Vazio,Escada,Plataforma],[Vazio,Escada,Plataforma,Alcapao]]
+[(1.0,0.0),(1.0,1.0)]
+-}
+posicaoBlocosesc :: Posicao -> [[Bloco]] -> [Posicao]
+posicaoBlocosesc _ [] = []
+posicaoBlocosesc (x, y) (h:t) = posicaoesc (x, y) h ++ posicaoBlocosesc (x, y + 1) t
+
+
+
+{-| Função que dada uma posição inicial e uma lista de Blocos(o que corresponde a uma linha de Blocos) dá a posição das escadas.
+
+== Exemplos
+
+>>>posicaoal (0.0,0.0) [Vazio,Escada,Plataforma,Alcapao]
+[(3.0,0.0)] 
+-}
+posicaoesc :: Posicao -> [Bloco] -> [Posicao]
+posicaoesc _ [] = []
+posicaoesc (x, y) (h:t)
+    | h == Escada = (x, y) : posicaoesc (x + 1, y) t
+    | otherwise = posicaoesc (x + 1, y) t 
+
+
+
+
+
+
+
+
+{-| Função que dada uma posição inicial e uma lista de listas de Blocos(cada lista de Blocos corresponde a uma linha ) dá a posição de todos os alçapoes.
+
+== Exemplos
+
+>>> posicaoBlocosal (0.0,0.0) [[Vazio,Escada,Plataforma],[Vazio,Escada,Plataforma,Alcapao]]
+[(3.0,1.0)]
+-}
+posicaoBlocosal :: Posicao -> [[Bloco]] -> [Posicao]
+posicaoBlocosal _ [] = []
+posicaoBlocosal (x, y) (h:t) = posicaoal (x, y) h ++ posicaoBlocosal (x, y + 1) t
+
+
+
+{-| Função que dada uma posição inicial e uma lista de Blocos(o que corresponde a uma linha de Blocos) dá a posição dos alçapoes.
+
+== Exemplos
+
+>>> posicaoal (0.0,0.0) [Vazio,Escada,Plataforma,Alcapao]
+[(3.0,0.0)]
+-}
+posicaoal :: Posicao -> [Bloco] -> [Posicao]
+posicaoal _ [] = []
+posicaoal (x, y) (h:t)
+    | h == Alcapao = (x, y) : posicaoal (x + 1, y) t
+    | otherwise = posicaoal (x + 1, y) t 
+
+
+
+
+
+
+
+
+
+{-| Função que dada uma posição inicial e uma lista de listas de Blocos(cada lista de Blocos corresponde a uma linha ) dá a posição de todas as plataformas.
+
+== Exemplos
+
+>>> posicaoBlocospl (0.0,0.0) [[Vazio,Escada,Plataforma],[Vazio,Escada,Plataforma,Alcapao]]
+[(2.0,0.0),(2.0,1.0)]
+-}
+posicaoBlocospl :: Posicao -> [[Bloco]] -> [Posicao]
+posicaoBlocospl _ [] = []
+posicaoBlocospl (x, y) (h:t) = posicaopl (x, y) h ++ posicaoBlocospl (x, y + 1) t
+
+
+
+{-| Função que dada uma posição inicial e uma lista de Blocos(o que corresponde a uma linha de Blocos) dá a posição das plataformas.
+
+== Exemplos
+
+>>> posicaopl (0.0,0.0) [Vazio,Escada,Plataforma,Alcapao]
+[(2.0,0.0)]
+-}
+posicaopl :: Posicao -> [Bloco] -> [Posicao]
+posicaopl _ [] = []
+posicaopl (x, y) (h:t)
+    | h == Plataforma = (x, y) : posicaopl (x + 1, y) t
+    | otherwise = posicaopl (x + 1, y) t 
+
