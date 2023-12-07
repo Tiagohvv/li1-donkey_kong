6eb08a0cdb55baf610d4e239dd58b230acfdefd6
Tiago <a106883@alunos.uminho.pt>
2023-12-07 10:45:18 +0000

    src

---
+++ src/desenha.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Desenha
+Description : Desenha o jogo
+Copyright   : José Miguel Torrinha Paredes Pinho Sampaio <a106908@alunos.uminho.pt>
+              Tiago Filipe Sousa Rodrigues Faria Alves <a106883@alunos.uminho.pt>
+
+Módulo para a realização de LI1 em 2023/24.
+-}
+module Desenha where
+
+    
---
+++ src/tempo.hs
@@ -0,0 +1,9 @@
+{-|
+Module      : Tempo
+Description : Tempo
+Copyright   : José Miguel Torrinha Paredes Pinho Sampaio <a106908@alunos.uminho.pt>
+              Tiago Filipe Sousa Rodrigues Faria Alves <a106883@alunos.uminho.pt>
+
+Módulo para a realização de LI1 em 2023/24.
+-}
+module Tempo where
